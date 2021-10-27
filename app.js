b90f3907050f167152941ddc8712e4d11d983bce
Jhon Christian Rey Tolop <jhonchristianreyt@gmail.com>
2021-10-27 14:57:28 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'});
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine', 'hbs');
+
+db.connect((err) => {
+    if (err) {
+        console.log("error");
+    }
+
+    else {
+        console.log("MYSQL Connected...");
+    }
+})
+
+app.get("/", (req, res) => {
+    // res.send("<h1>BISU Main Campus Contact Tracing System</h1>")
+    res.render("index")
+});
+
+app.listen(3000, () => {
+    console.log("Server started in Port 3000.");
+})
